19760e281992877a38d1c968511e12fa9f6d5ef1
jasminethai-hub <jtt9fb@umsystem.edu>
2023-10-07 14:51:01 -0500

    controller

--- IncomeExpense_JavaSpring/src/main/java/TeamHaLoi/IncomeExpenseTracker/controller/UserAccountController.java
+++ IncomeExpense_JavaSpring/src/main/java/TeamHaLoi/IncomeExpenseTracker/controller/UserAccountController.java
@@ -1,10 +1,63 @@
 package TeamHaLoi.IncomeExpenseTracker.controller;
 
-import TeamHaLoi.IncomeExpenseTracker.exception.UserAccountNotFoundException;
 import TeamHaLoi.IncomeExpenseTracker.model.UserAccount;
 import TeamHaLoi.IncomeExpenseTracker.repository.UserAccountRepository;
+import TeamHaLoi.IncomeExpenseTracker.exception.UserAccountNotFoundException; // Ensure you create this exception class
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.util.List;
+
+@RestController
+@RequestMapping("/user_accounts")  // Set the base URL for all endpoints in this controller
+public class UserAccountController {
+
+    @Autowired
+    UserAccountRepository userAccountRepository;
+
+    // Get All UserAccounts
+    @GetMapping
+    public List<UserAccount> getAllUserAccounts() {
+        return userAccountRepository.findAll();
+    }
+
+    // Create a new UserAccount
+    @PostMapping
+    public UserAccount createUserAccount(@Valid @RequestBody UserAccount userAccount) {
+        return userAccountRepository.save(userAccount);
+    }
+
+    // Get a Single UserAccount
+    @GetMapping("/{id}")
+    public UserAccount getUserAccountById(@PathVariable(value = "id") Long userId) throws UserAccountNotFoundException {
+        return userAccountRepository.findById(userId)
+                .orElseThrow(() -> new UserAccountNotFoundException(userId));
+    }
+
+    // Update a UserAccount
+    @PutMapping("/{id}")
+    public UserAccount updateUserAccount(@PathVariable(value = "id") Long userId,
+                                         @Valid @RequestBody UserAccount userAccountDetails) throws UserAccountNotFoundException {
+
+        UserAccount userAccount = userAccountRepository.findById(userId)
+                .orElseThrow(() -> new UserAccountNotFoundException(userId));
+
+        userAccount.setEmail(userAccountDetails.getEmail());
+        userAccount.setPassword(userAccountDetails.getPassword());
+        userAccount.setFull_name(userAccountDetails.getFull_name());
+
+        return userAccountRepository.save(userAccount);
+    }
+
+    // Delete a UserAccount
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUserAccount(@PathVariable(value = "id") Long userId) throws UserAccountNotFoundException {
+        UserAccount userAccount = userAccountRepository.findById(userId)
+                .orElseThrow(() -> new UserAccountNotFoundException(userId));
+
+        userAccountRepository.delete(userAccount);
+
+        return ResponseEntity.ok().build();
+    }
+}
