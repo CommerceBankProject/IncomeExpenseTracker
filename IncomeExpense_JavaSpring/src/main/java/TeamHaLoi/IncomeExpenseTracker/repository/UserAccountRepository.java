b1ff627234379d0c25e40f2ff5d6ba75c13e27bb
jasminethai-hub <jtt9fb@umsystem.edu>
2023-10-07 15:26:24 -0500

    repo

--- IncomeExpense_JavaSpring/src/main/java/TeamHaLoi/IncomeExpenseTracker/repository/UserAccountRepository.java
+++ IncomeExpense_JavaSpring/src/main/java/TeamHaLoi/IncomeExpenseTracker/repository/UserAccountRepository.java
@@ -2,13 +2,10 @@ package TeamHaLoi.IncomeExpenseTracker.repository;
 import TeamHaLoi.IncomeExpenseTracker.model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 
 @Repository
-@RequestMapping("/api/users")
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long>{
 
 }
